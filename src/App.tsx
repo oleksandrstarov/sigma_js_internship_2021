23bf8597176c2824cd5f0508a118cadbfa0d78af
Dmitriy Kurchenko <dmitriy1991dp@gmail.com>
2021-03-30 17:12:50 +0300

    routing has been created for pages such as Home, DetailsPage and SearchResult Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import DetailsPage from "./pages/DetailsPage";
+import SearchResults from "./pages/SearchResults";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/details-page">Details Page</Link>
+        </li>
+        <li>
+          <Link to="/searh-results">Search Results</Link>
+        </li>
+        <li>
+          <Link to="/favorites">Favorites</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/details-page" component={DetailsPage} />
+        <Route path="/searh-results" component={SearchResults} />
+        <Route path="/favorites" component={SearchResults} />
+      </Switch>
+    </Router>
   );
 }
 
