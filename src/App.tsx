7a39362e48f5b2747f3fc3248434fa699284a218
Oleksandr <oleksandr.starov@gmail.com>
2021-03-30 18:14:47 +0300

    Revert "routing has been created for pages such as Home, DetailsPage and SearchResult Page (#38)" (#47)
    This reverts commit 4063f85ea74d1788589c260a7052baabaa5ce677.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import DetailsPage from "./pages/DetailsPage";
-import SearchResults from "./pages/SearchResults";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/details-page">Details Page</Link>
-        </li>
-        <li>
-          <Link to="/searh-results">Search Results</Link>
-        </li>
-        <li>
-          <Link to="/favorites">Favorites</Link>
-        </li>
-      </ul>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/details-page" component={DetailsPage} />
-        <Route path="/searh-results" component={SearchResults} />
-        <Route path="/favorites" component={SearchResults} />
-      </Switch>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
