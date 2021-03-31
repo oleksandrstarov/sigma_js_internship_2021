761c387c1c859f2b54b21073ae8b0b9e31282cfc
di-shu <dmitry.shulyachenko@gmail.com>
2021-03-31 17:10:41 +0300

    fixed routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import "./App.css";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import DetailsPage from "./pages/DetailsPage";
-import MoviesList from "./pages/MoviesList";
+import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import DetailsPage from './pages/DetailsPage';
+import MoviesList from './pages/MoviesList';
 
 
 function App() {
@@ -11,23 +11,27 @@ function App() {
     <Router>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to='/'>Home</Link>
         </li>
         <li>
-          <Link to="/details-page">Details Page</Link>
+          <Link to='/movie-details'>Details Page</Link>
         </li>
         <li>
-          <Link to="/search-results?title=?isFavorite=?minYear=?maxYear=?genre=">Search Results</Link>
+          <Link to='/search-results/:title?:id?'>Search Results</Link>
         </li>
         <li>
-          <Link to="/favorites">Favorites</Link>
+          <Link to='/favorites'>Favorites</Link>
         </li>
       </ul>
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/details-page" component={DetailsPage} />
-        <Route path="/search-results" component={MoviesList} />
-        <Route path="/favorites" component={MoviesList} />
+        <Route exact path='/' component={Home} />
+        <Route path='/movie-details' component={DetailsPage} />
+        <Route path='/favorites'>
+          <MoviesList title="Favorites"/>
+        </Route>
+        <Route path='/search-results/:title?:id?'>
+          <MoviesList title="Search Results"/>
+        </Route>
       </Switch>
     </Router>
   );
--- src/pages/MoviesList.tsx
+++ src/pages/MoviesList.tsx
@@ -1,7 +1,24 @@
+import { ReactNode } from "react";
+import { useParams } from "react-router";
+
 const MoviesList = (props:any) => {
+    type Query = {
+        title: string,
+        id: string
+    }
+
+    type Props = {
+        title: string;
+        children?: ReactNode;
+    }
+
+    const query :Query = useParams();
+    const properties :Props = props;
+
    return (
       <div>
-         <h1>{props.title}</h1>
+          <h1>{properties.title}</h1>
+          <p>title query: {query.title}</p>
       </div>
    )
 }
