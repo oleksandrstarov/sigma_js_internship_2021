307b30bdd33ff4e7307c2f94e05f3a2a80df00a0
di-shu <dmitry.shulyachenko@gmail.com>
2021-04-02 02:18:07 +0300

    deleted routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-import Home from './components/Home';
-import DetailsPage from './components/MovieDetails';
-import Favorites from './components/Favorites';
-import SearchResults from './components/SearchResults';
+import logo from './logo.svg';
 
 import './App.css';
 
 function App() {
     return (
-      <Router>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/movie-details/:id' component={DetailsPage} />
-          <Route path='/favorites' component={Favorites}/>
-          <Route path='/search-results/:title?&:genre?&:minYear?&:maxYear?' component={SearchResults}/>
-        </Switch>
-      </Router>
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
     )
 }
 
