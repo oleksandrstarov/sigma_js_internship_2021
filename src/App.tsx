8393945dd0077a8f6fa2cb76fb951ff9f332f24f
di-shu <dmitry.shulyachenko@gmail.com>
2021-04-02 02:09:25 +0300

    rollback device-hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,21 @@
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-
-import Home from './components/Home';
-import DetailsPage from './components/MovieDetails';
-import Favorites from './components/Favorites';
-import SearchResults from './components/SearchResults';
+import logo from './logo.svg';
 
 import './App.css';
 
-
 function App() {
     return (
-      <Router>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/movie-details'>Movie details</Link>
-          </li>
-          <li>
-            <Link to='/search-results/:title'>Search Results</Link>
-          </li>
-          <li>
-            <Link to='/favorites'>Favorites</Link>
-          </li>
-        </ul>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/movie-details/:id' component={DetailsPage} />
-          <Route path='/favorites' component={Favorites}/>
-          <Route path='/search-results/:title?&:genre?&:minYear?&:maxYear?' component={SearchResults}/>
-        </Switch>
-      </Router>
-    )
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
-
 
 export default App;
