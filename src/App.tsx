147d705f1b8dbac1eba37415de4219cfe5509bef
di-shu <54156812+di-shu@users.noreply.github.com>
2021-04-02 18:20:06 +0300

    Feature/routing (#51)
    * routing has been created for pages such as Home, DetailsPage and SearchResult Page
    
    * fixed link search-results
    
    * Adds prettier and husky hooks
    
    * cleans up constants.js
    
    * init
    
    * Updated tsconfig.json
    
    * adds new line
    
    * changed width of formatting
    
    * updated config
    
    * init
    
    * fixed routes
    
    * changed to single quotes
    
    * fixed tsconfig file
    
    * solved mistakes: route params, components props, formatting
    
    * rollback routing
    
    * removed query params
    
    Co-authored-by: Dmitriy Kurchenko <dmitriy1991dp@gmail.com>
    Co-authored-by: Oleksandr Starov <oleksandr.starov@leovegas.com>
    Co-authored-by: Dima Shulyachenko <proghammer@MacBook-Air-Dima.local>

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Home from './components/Home';
+import MovieDetails from './components/MovieDetails';
+import Favorites from './components/Favorites';
+import SearchResults from './components/SearchResults';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <Router>
+        <Switch>
+          <Route exact path='/' component={ Home } />
+          <Route path='/movie-details/:id' component={ MovieDetails } />
+          <Route path='/favorites' component={ Favorites }/>
+          <Route path='/search-results' component={ SearchResults }/>
+        </Switch>
+      </Router>
+    )
 }
 
 export default App;
---
+++ src/components/SearchResults.tsx
@@ -0,0 +1,12 @@
+const SearchResults = () => {
+   return (
+      <div className='search-wrapper'>
+          <h1>Results</h1>
+          <div className="movie-list">
+
+          </div>
+      </div>
+   )
+}
+
+export default SearchResults;
