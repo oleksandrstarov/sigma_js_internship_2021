04e3b3e2422a92b031388d62712d44b157259b99
di-shu <dmitry.shulyachenko@gmail.com>
2021-04-07 00:41:59 +0300

    added dynamic propety to Breadcrunms

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -1,27 +1,41 @@
 import '../styles/Breadcrumbs.scss';
+import { useEffect, useState } from 'react';
 
 type BreadcrumbsProps = {
-    dynamic?: boolean
-    className?: string;
-    children: any
+  dynamic?: boolean;
+  className?: string;
+  children: any;
 };
 
 const Breadcrumbs = ({ className, children, dynamic }: BreadcrumbsProps) => {
-    const generateLinks = () => {
-        const items = children.map((item:any, index:number) => {
-            return <a className="breadcrumb" href={item.path} key={index}>{item.name}</a>
-        })
+  const [prevPathName, setPrevPathname] = useState('');
 
-        return items;
-    }
+  useEffect(() => {
+    setPrevPathname(`/${document.referrer.split('/')[3]}`);
+  });
 
-    return(
-        <div className={`breadcrumbs ${ className }`}>
-            {
-                generateLinks()
-            }
-        </div>
-    )
+  const generateLinks = () => {
+    const items = children.map((item: any, index: number) => {
+      return (
+        <a className="breadcrumb" href={item.path} key={index++}>
+          {item.name}
+        </a>
+      );
+    });
+
+    return items;
+  };
+
+  return (
+    <div className={`breadcrumbs ${className}`}>
+      {dynamic && (
+        <a className="breadcrumb" href={prevPathName} key={0}>
+          {prevPathName}
+        </a>
+      )}
+      {generateLinks()}
+    </div>
+  );
 };
 
 export default Breadcrumbs;
