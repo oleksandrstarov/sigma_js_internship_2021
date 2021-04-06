b34c60b1fde3f6bb6603cce0f0ef40cbf1dce7bc
di-shu <dmitry.shulyachenko@gmail.com>
2021-04-06 22:52:15 +0300

    created Breadcrumbs component

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -1,15 +1,25 @@
+import '../styles/Breadcrumbs.scss';
+
 type BreadcrumbsProps = {
+    dynamic?: boolean
     className?: string;
+    children: any
 };
 
-const Breadcrumbs = ({ className }: BreadcrumbsProps) => {
-    const generateLinks = (): string => {
-        return 'breadcrumbs';
+const Breadcrumbs = ({ className, children, dynamic }: BreadcrumbsProps) => {
+    const generateLinks = () => {
+        const items = children.map((item:any, index:number) => {
+            return <a className="breadcrumb" href={item.path} key={index}>{item.name}</a>
+        })
+
+        return items;
     }
 
     return(
         <div className={`breadcrumbs ${ className }`}>
-            { generateLinks() }
+            {
+                generateLinks()
+            }
         </div>
     )
 };
