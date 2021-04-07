9ccc4b2a3063e87792664ca022a85fb5832583e9
Kachyr <56648514+Kachyr@users.noreply.github.com>
2021-04-07 12:53:23 +0300

    bugfix for layout and movie details (#95)
    Co-authored-by: Kach <kach_work@ukr.net>

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,23 @@ import Home from './components/Home';
 import MovieDetails from './components/MovieDetails';
 import Favorites from './components/Favorites';
 import SearchResults from './components/SearchResults';
-import Header from './components/Header';
+import Layout from './components/Layout';
 
 import './App.scss';
 
 function App() {
   return (
     <>
-      <Header />
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/movie-details/:id" component={MovieDetails} />
-          <Route path="/favorites" component={Favorites} />
-          <Route path="/search-results" component={SearchResults} />
-        </Switch>
-      </Router>
+      <Layout>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/movie-details/:id" component={MovieDetails} />
+            <Route path="/favorites" component={Favorites} />
+            <Route path="/search-results" component={SearchResults} />
+          </Switch>
+        </Router>
+      </Layout>
     </>
   );
 }
