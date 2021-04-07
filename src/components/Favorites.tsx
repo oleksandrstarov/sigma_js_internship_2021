796b4982a17970d7cc60d61bc7eca3f9a0c64615
Ilia Selieznov <ziziron21@gmail.com>
2021-04-07 13:28:58 +0300

    deleted execly the same comment in this branch

--- src/components/Favorites.tsx
+++ src/components/Favorites.tsx
@@ -1,13 +1,7 @@
 
-// import from './';
-
-// import CardInfo from "./CardInfo"
-
 const Favorites = () => {
   return (
     <div className="favorites-wrapper">
-      {/* < pageTitle="Favorites" /> */}
-      {/* <CardInfo number={537056} /> */}
     </div>
   );
 };
