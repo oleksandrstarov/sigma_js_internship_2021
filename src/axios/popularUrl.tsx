2e79309c368576ff92fd9adcc282e674128980c8
Ilia Selieznov <ziziron21@gmail.com>
2021-04-07 21:58:06 +0300

    after pull

--- src/axios/popularUrl.tsx
+++ src/axios/popularUrl.tsx
@@ -1,6 +1,9 @@
 import axios from 'axios';
+
 import { API_POPULAR_URL } from '../constants/api';
+
 const url = axios.create({
     baseURL: `${API_POPULAR_URL}`
 });
+
 export default url;
--- src/axios/searchUrl.tsx
+++ src/axios/searchUrl.tsx
@@ -1,6 +1,9 @@
 import axios from 'axios';
+
 import { API_SEARCH_URL } from '../constants/api';
+
 const url = axios.create({
     baseURL: `${API_SEARCH_URL}`
 });
+
 export default url;
