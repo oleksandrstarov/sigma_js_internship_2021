3ce89f325d2e5a018c3f368efa43580c971df458
Ilia Selieznov <ziziron21@gmail.com>
2021-04-08 14:21:17 +0300

    fixed comments

--- src/components/CardInfo.tsx
+++ src/components/CardInfo.tsx
@@ -1,10 +1,15 @@
 import { useEffect, useState } from 'react';
+
 import { NavLink } from 'react-router-dom';
+
 import buttonImgSrc from '../assets/image84.png';
+
 import api from 'src/service/api';
+
 import '../styles/CardInfo.scss';
 
 let CardInfo = ({ tailWide, number }: { number: number; tailWide: boolean }) => {
+
     const [data, setData] = useState({
         poster_path: '',
         original_title: '',
@@ -13,7 +18,9 @@ let CardInfo = ({ tailWide, number }: { number: number; tailWide: boolean }) => 
         vote_count: '',
         overview: ''
     });
+
     const { poster_path, original_title, title, vote_average, overview } = data;
+
     const srcImg = api.changeImgLinks(poster_path, 'w342');
 
     useEffect(() => {
@@ -24,8 +31,11 @@ let CardInfo = ({ tailWide, number }: { number: number; tailWide: boolean }) => 
 
     return (
         <div className={!tailWide ? 'card-info' : 'card-info card-info__tail'}>
+
             {/* temporary TitleComponent */}
+
             <div className='card-info__wrapper'>
+
                 {!tailWide ? (
                     <div className='titleComponent card-info__title'>
                         <h2>{title}</h2>
@@ -35,38 +45,59 @@ let CardInfo = ({ tailWide, number }: { number: number; tailWide: boolean }) => 
                         <h2>{title}</h2>
                     </div>
                 )}
+
                 {!tailWide ? null : (
+
                     <div className='card-info__description'>
+
                         {overview.length >= 250 ? (
                             <p>
-                                {' '}
                                 {overview.slice(0, 250)}
+
                                 <NavLink to={`/movie-details/:${number} `}>
+
                                     <span>...Read more</span>
+
                                 </NavLink>
+
                             </p>
                         ) : (
                             overview
                         )}
+
                     </div>
                 )}
+
             </div>
+
             <div className='cars-info_container'>
+
                 <img src={srcImg} alt={original_title} className={'card-info__img'} />
+
                 <div className='card-info__gradient' />
+
                 <div className='info-card__rate'>
+
                     <div className='info-card__imdb'>IMDB {vote_average}</div>
+
                     <div className='info-card__voters'>Voters {vote_average}</div>
+
                 </div>
+
                 <NavLink to={`/movie-details/:${number}`}>
+
                     <button type='button' className={'card-info__button'}>
+
                         <div className={'card-info__button-view'}>
+
                             <p> {'VIEW DETAILS'}</p>
+
                             <img
                                 src={buttonImgSrc}
                                 alt={original_title}
                                 className={'card-info__button-img'}
                             />
+
                         </div>
                     </button>
                 </NavLink>
@@ -74,4 +105,5 @@ let CardInfo = ({ tailWide, number }: { number: number; tailWide: boolean }) => 
         </div>
     );
 };
+
 export default CardInfo;
