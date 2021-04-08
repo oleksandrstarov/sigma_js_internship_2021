c51c8450e54af0cdb0d1e4fa13cd3fdc0ac04597
Ilia Selieznov <ziziron21@gmail.com>
2021-04-08 16:53:10 +0300

    fixed commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-<<<<<<< HEAD
-import Home from './components/Home';
-import Favorites from './components/Favorites';
-import SearchResults from './components/SearchResults';
-import Layout from './components/Layout';
-import MovieDetails from './components/MovieDetails';
-=======
 import Layout from './components/Layout';
 import routing from './config/routing';
 
->>>>>>> 45afd35d4fe293f8ecd0bee8a96e8e271fc734c9
 import './App.scss';
 
 function App() {
