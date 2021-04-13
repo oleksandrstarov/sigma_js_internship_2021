2375c4d99861a50c1d412e8386d482854a3d3604
Ilia Selieznov <ziziron21@gmail.com>
2021-04-13 10:07:09 +0300

    small card with style like on demo

--- src/components/SmallInfoCard.tsx
+++ src/components/SmallInfoCard.tsx
@@ -1,19 +1,39 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import api from 'src/service/api';
 
 import buttonImgSrc from '../assets/image95.png';
 import '../styles/SmallCardInfo.scss';
 
-const SmallInfoCard = () => {
 
-  const [data, setData] = useState<{}>();
+type CardinfoProps = {
+  id: number;
+};
+
+type DataType = {
+  title: string;
+  poster_path: string;
+  vote_average: string;
+  vote_count: string;
+  id?: number;
+};
+
+const SmallInfoCard = ({ id }: CardinfoProps) => {
+
+  const [data, setData] = useState<DataType>({
+    poster_path: '',
+    vote_count: '',
+    title: '',
+    vote_average: '',
+
+  });
 
   useEffect(() => {
-    api.getDataById(123).then((res: any) => {
+    api.getDataById(id).then((res) => {
       setData(res);
     });
-  }, [])
+  }, []);
 
   return (
     <div className="small-cardInfo">
@@ -22,17 +42,19 @@ const SmallInfoCard = () => {
         <p>{data.title}</p>
       </div>
       <div className="small-cardInfo__rate">
+        <div className="small-cardInfo__rate-voters">Voters<br />{data.vote_count}</div>
         <div className="small-cardInfo__rate-imdb">IMDB<br />{data.vote_average}</div>
-        <div className="small-cardInfo__rate-voters">Voters<br />{data.vote_average}</div>
       </div>
       <div className="small-cardInfo__filter"></div>
-      <img src={`https://image.tmdb.org/t/p/w185${data.poster_path}`} alt={data.title} className="smallCardInfo__img" />
-      <link rel="stylesheet" href={`/movie-details/${data.id}`}>
+      <div className="small-cardInfo__img-wrapper">
+        <img src={`https://image.tmdb.org/t/p/w185${data.poster_path}`} alt={data.title} className="small-cardInfo__img" />
+      </div>
+      <Link to={`/movie-details/${data.id}`}>
         <button className="small-cardInfo__button">
           <p className="small-cardInfo__button-text">VIEW DETAILS</p>
           <img src={buttonImgSrc} alt="button Img" className="small-cardInfo__button-img" />
         </button>
-      </link>
+      </Link>
     </div >
   )
 }
