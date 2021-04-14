e839982909d3c8868bad9c115765a9ca690d1d98
Solomiia Sahala <35579102+solomiia-sahala@users.noreply.github.com>
2021-04-14 21:27:04 +0300

    Implemented movie details page (#118)
    * pulled from main
    
    * implemented read more functionality
    
    * added styles for movie details and created fetch request to get data
    
    * fixed comments
    
    * added types to variables with typescript and fixed comments
    
    * changed interface to type and updated his name

--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -1,9 +1,120 @@
-const MovieDetails = () => {
-  return (
-    <div>
-      <p>Movie details</p>
-    </div>
-  );
+import { useState, useEffect, ReactNode } from 'react';
+
+import api from '../service/api';
+
+import ReadMore from './ReadMore';
+
+import '../styles/MovieDetails.scss';
+
+enum ImageWidth {
+  w500
+}
+
+type MovieDetailsProps = {
+  match: { params: { id: string } }
+}
+
+type MovieInfo = {
+  poster_path: string;
+  original_title: string;
+  title: string;
+  tagline: string;
+  status: string;
+  release_date: string;
+  budget: string;
+  popularity: string;
+  vote_average: string;
+  runtime: string;
+  genres: [{ id: number | null, name: string }],
+  production_countries: [{ name: string }]
+  overview: string;
 };
+
+const MovieDetails = ({ match }: MovieDetailsProps) => {
+  const [movieData, setMovieData] = useState<MovieInfo>({
+    poster_path: '',
+    original_title: '',
+    title: '',
+    tagline: '',
+    status: '',
+    release_date: '',
+    budget: '',
+    popularity: '',
+    vote_average: '',
+    runtime: '',
+    genres: [{ id: null, name: '' }],
+    production_countries: [{ name: '' }],
+    overview: '',
+  })
+
+  const {
+    poster_path,
+    original_title,
+    title,
+    tagline,
+    status,
+    release_date,
+    budget,
+    popularity,
+    vote_average,
+    runtime,
+    genres,
+    production_countries,
+    overview
+  } = movieData;
+
+  const poster = api.changeImgLinks(poster_path, ImageWidth[0]);
+
+  useEffect(() => {
+    api.getDataById(Number(match.params.id)).then((res: any) => {
+      setMovieData(res);
+    })
+  }, [])
+
+  const renderMovieInfo = (header: string, info: string | number): ReactNode => {
+    return (
+      <tr>
+        <td><h4>{header}: </h4></td>
+        <td><p>{info}</p></td>
+      </tr>
+    )
+  }
+
+  return (
+    <div className="details-container">
+      <section className="movie-wrapper">
+        <div className="movie-img">
+          <img src={poster} alt="poster" />
+        </div>
+        <div className="movie-details">
+          <h1>{title}</h1>
+          <div className="general-info">
+            <table>
+              {renderMovieInfo("Original title", original_title)}
+              {renderMovieInfo("Tagline", tagline)}
+              {renderMovieInfo("release_date", release_date)}
+              {renderMovieInfo("Status", status)}
+              {renderMovieInfo("Budget", budget)}
+              {renderMovieInfo("Country", production_countries[0].name)}
+              {renderMovieInfo("Duration", runtime)}
+              {renderMovieInfo("IMDB", vote_average)}
+              {renderMovieInfo("Popularity", popularity)}
+            </table>
+          </div>
+        </div>
+      </section>
+      <div className="genres">
+        {genres && genres.map((genre) => <div key={genre.id}>{genre.name}</div>)}
+      </div>
+      <div className="hl"></div>
+      <div className="description">
+        <ReadMore>
+          {overview}
+        </ReadMore>
+      </div>
+      <div className="hl"></div>
+    </div>
+  )
+}
 
 export default MovieDetails;
