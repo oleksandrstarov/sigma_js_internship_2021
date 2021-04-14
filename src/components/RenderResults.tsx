1dbdaf5ad4d2462365d592247b5268b524ee5aca
mrSelieznov <41084902+mrSelieznov@users.noreply.github.com>
2021-04-14 21:08:12 +0300

    Feature/Render results and  tail toggler (#105)
    * added assets, renderResults component and scss
    
    * added main styles,desktop results , new style for card, updated service
    
    * fixed comments
    
    * fixed commit
    
    * fixed comments

---
+++ src/components/RenderResults.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+
+import buttonImgLine from '../assets/lines.png';
+import buttonImgGroup from '../assets/Group.png';
+
+import '../styles/RenderResults.scss';
+import CardInfo from './CardInfo';
+import { MovieCard } from '../models/index';
+
+interface RenderResultsArrList {
+  list: MovieCard[];
+  original_title: string;
+  vote_count: number;
+}
+
+const RenderResults = ({ list }: RenderResultsArrList) => {
+  const [tailState, setTailState] = useState(true);
+
+  const handlerTail = () => setTailState(!tailState);
+
+  return (
+    <div className="delivery">
+      <div className="view-toggler">
+        {tailState ? (
+          <img
+            src={buttonImgGroup}
+            alt="button-img-group"
+            className="delivery__toggler"
+            onClick={() => handlerTail()}
+          />
+        ) : (
+          <img
+            src={buttonImgLine}
+            alt="button-img-line"
+            className="delivery__toggler"
+            onClick={() => handlerTail()}
+          />
+        )}
+      </div>
+      <div className="delivery__container">
+        {list.map((item: MovieCard) => {
+          return <CardInfo tailWide={!tailState} number={item.id} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default RenderResults;
