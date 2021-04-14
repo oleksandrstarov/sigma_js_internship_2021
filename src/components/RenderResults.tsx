c05e39971046e4ca634493eea46aafdaaaff7487
Ilia Selieznov <ziziron21@gmail.com>
2021-04-14 17:57:58 +0300

    fixed commit

--- src/components/RenderResults.tsx
+++ src/components/RenderResults.tsx
@@ -5,27 +5,41 @@ import buttonImgGroup from '../assets/Group.png';
 
 import '../styles/RenderResults.scss';
 import CardInfo from './CardInfo';
+import { MovieCard } from '../models/index';
 
-type ArrListProps = {
-  list: {}[];
+interface RenderResultsArrList {
+  list: MovieCard[];
+  original_title: string;
+  vote_count: number;
 }
 
-const RenderResults = ({ list }: ArrListProps) => {
-
+const RenderResults = ({ list }: RenderResultsArrList) => {
   const [tailState, setTailState] = useState(true);
 
   const handlerTail = () => setTailState(!tailState);
 
   return (
-    <div className='delivery'>
-      <div className='view-toggler'>
-        {tailState
-          ? <img src={buttonImgGroup} alt='button-img-group' className='delivery__toggler' onClick={() => handlerTail()} />
-          : <img src={buttonImgLine} alt='button-img-line' className='delivery__toggler' onClick={() => handlerTail()} />}
+    <div className="delivery">
+      <div className="view-toggler">
+        {tailState ? (
+          <img
+            src={buttonImgGroup}
+            alt="button-img-group"
+            className="delivery__toggler"
+            onClick={() => handlerTail()}
+          />
+        ) : (
+          <img
+            src={buttonImgLine}
+            alt="button-img-line"
+            className="delivery__toggler"
+            onClick={() => handlerTail()}
+          />
+        )}
       </div>
       <div className="delivery__container">
-        {list.map((item: any) => {
-          return <CardInfo tailWide={!tailState} number={item.id} />
+        {list.map((item: MovieCard) => {
+          return <CardInfo tailWide={!tailState} number={item.id} />;
         })}
       </div>
     </div>
--- src/service/api.tsx
+++ src/service/api.tsx
@@ -1,19 +1,19 @@
-import {
-  API_KEY,
-  API_IMG_URL
-} from '../constants/api';
+import { API_KEY, API_IMG_URL } from '../constants/api';
 import axios from '../axios/url';
 
 import { Theme } from '../models/index';
 
-const apiService: { storeKey: string, store: { history: number[], favorites: number[], theme: any }, } = {
+const apiService: {
+  storeKey: string;
+  store: { history: number[]; favorites: number[]; theme: any };
+} = {
   storeKey: 'service',
   store: {
     history: [],
     favorites: [],
     theme: Theme.light
-  },
-}
+  }
+};
 
 const api = {
   getStore() {
@@ -25,17 +25,19 @@ const api = {
     return apiService.store;
   },
 
-  setStore(data: Object = {
-    history: [],
-    favorites: [],
-    theme: 1
-  }) {
+  setStore(
+    data: Object = {
+      history: [],
+      favorites: [],
+      theme: 1
+    }
+  ) {
     localStorage.setItem(apiService.storeKey, JSON.stringify(data));
   },
 
   switchTheme() {
     const store = this.getStore();
-    store.theme = (store.theme === Theme.light) ? Theme.dark : Theme.light;
+    store.theme = store.theme === Theme.light ? Theme.dark : Theme.light;
     this.setStore(store);
     return store.theme;
   },
@@ -44,8 +46,8 @@ const api = {
     const store = this.getStore();
     const isThereAnId = store.favorites.includes(id);
     if (!isThereAnId) {
-      store.favorites.push(id)
-    };
+      store.favorites.push(id);
+    }
     this.setStore(store);
   },
 
@@ -70,8 +72,8 @@ const api = {
     const store = this.getStore();
     const isThereAnId = store.history.includes(id);
     if (isThereAnId) {
-      store.history.push(id)
-    };
+      store.history.push(id);
+    }
     this.setStore(store);
   },
 
@@ -94,9 +96,10 @@ const api = {
   getDataByIds(ids: number[]) {
     const urls = ids.map((id: number) => `movie/${id}?${API_KEY}`);
     const requests = urls.map(
-      async (url: any) => await axios.get(url).then((res: { data: any; }) => res.data)
+      async (url: any) =>
+        await axios.get(url).then((res: { data: any }) => res.data)
     );
-    return Promise.all(requests)
+    return Promise.all(requests);
   },
 
   async getDataById(id: number) {
@@ -110,7 +113,9 @@ const api = {
   },
 
   async getTopRatedList() {
-    const obj = await axios.get(`movie/top_rated?translations&${API_KEY}&region=US`);
+    const obj = await axios.get(
+      `movie/top_rated?translations&${API_KEY}&region=US`
+    );
     return obj.data.results;
   },
 
@@ -121,7 +126,7 @@ const api = {
 
   changeImgLinks(url: string, size: string) {
     return `${API_IMG_URL}${size}${url}`;
-  },
-}
+  }
+};
 
 export default api;
