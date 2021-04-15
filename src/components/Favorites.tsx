089bd1a0815ae0d9d3ff875c0befcf2ecdc49abd
Ilia Selieznov <ziziron21@gmail.com>
2021-04-15 22:32:01 +0300

    fixed comments

--- src/components/Favorites.tsx
+++ src/components/Favorites.tsx
@@ -1,17 +1,25 @@
 import { useEffect, useState } from 'react';
 import api from 'src/service/api';
 import RenderResults from './RenderResults';
-
+type FaforitsApiData = {
+  poster_path: string;
+  original_title: string;
+  title: string;
+  vote_average: number;
+  vote_count: string;
+  overview: string;
+  backdrop_path: string;
+  id: number;
+}
 const Favorites = () => {
-
-  const [data, setData] = useState([{
-    poster_path: ' ',
-    original_title: ' ',
-    title: ' ',
+  const [data, setData] = useState<FaforitsApiData[]>([{
+    poster_path: '',
+    original_title: '',
+    title: '',
     vote_average: 0,
-    vote_count: ' ',
-    overview: ' ',
-    backdrop_path: ' ',
+    vote_count: '',
+    overview: '',
+    backdrop_path: '',
     id: 0,
   }]);
 
@@ -20,7 +28,6 @@ const Favorites = () => {
       setData(res);
     });
   }, [])
-  console.log(data);
 
   return (
     <div className="favorites-wrapper">
