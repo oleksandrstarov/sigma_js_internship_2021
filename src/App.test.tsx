b3a1518aeaf336520a4b1855ba3c70ce586dbecb
Oleksandr Starov <oleksandr.starov@leovegas.com>
2021-04-15 19:17:19 +0300

    fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const createdByEl = screen.getByText(/Created by students of sigma software/i);
+  expect(createdByEl).toBeInTheDocument();
 });
