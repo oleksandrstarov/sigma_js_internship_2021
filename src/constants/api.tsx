fda21e6af575574e429ed4076bbd9b8ff9b52b76
Ilia Selieznov <ziziron21@gmail.com>
2021-04-16 15:57:29 +0300

    fixed commmet

--- src/constants/api.tsx
+++ src/constants/api.tsx
@@ -3,17 +3,16 @@ export const API_URL = 'https://api.themoviedb.org/3/';
 export const API_IMG_URL = 'https://image.tmdb.org/t/p/';
 export const API_DATE_QUERY = 'primary_release_date.lte=';
 export const API_GENRE_ID = [
-    { id: 35, name: "Comedy" },
-    { id: 18, name: "Drama" },
-    { id: 10749, name: "Romance" },
-    { id: 9648, name: "Mystery" },
-    { id: 53, name: "Thriller" },
-    { id: 16, name: "Animation" },
-    { id: 28, name: "Action" },
-    { id: 12, name: "Adventure" },
-    { id: 80, name: "Crime" },
-    { id: 14, name: "Fantasy" },
-    { id: 878, name: "Science Fiction" },
-    { id: 27, name: "Horror" },
-    { id: 10751, name: "Family" }
+    { comedy: 35 },
+    { drama: 18 },
+    { romance: 10749 },
+    { mystery: 9648 },
+    { thriller: 53 },
+    { animation: 16 },
+    { action: 28 },
+    { adventure: 12 },
+    { crime: 80 },
+    { fantasy: 14 },
+    { 'Science Fiction': 28 },
+    { Family: 10751 },
 ]
