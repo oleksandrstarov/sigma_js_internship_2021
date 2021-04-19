2af4bcba73895d63ecc788446e6a0c4f3ecc7d73
Kach <kach_work@ukr.net>
2021-04-19 16:59:40 +0300

    refactoring of movie description

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -1,27 +1,20 @@
 import '../styles/Detail.scss';
 
-type DetailProps =
-  | {
-      title: string;
-      textContent?: string | number;
-      arrayOfContent: { name: string }[];
-    }
-  | {
-      title: string;
-      textContent: string | number;
-      arrayOfContent?: { name: string }[];
-    };
+type DetailProps = {
+  title: string;
+  textContent: string | number | undefined | null;
+};
 
-const Detail = ({ title, textContent, arrayOfContent }: DetailProps) => {
+const Detail = ({ title, textContent }: DetailProps): JSX.Element => {
   return (
-    <div className="detail-body">
-      <div className="detail-title">{title}:</div>
-      <div>
-        {arrayOfContent
-          ?.map((item: { name: string | number }) => item.name)
-          .join(', ') || textContent}
-      </div>
-    </div>
+    <>
+      {textContent ? (
+        <div className="detail-body">
+          <h4 className="detail-title">{title}:</h4>
+          <div>{textContent}</div>
+        </div>
+      ) : null}
+    </>
   );
 };
 
