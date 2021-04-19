04689f42bc6f0065f40e26f4c91217694b6a6cdc
di-shu <dmitry.shulyachenko@gmail.com>
2021-04-19 23:37:41 +0300

    created preloader

---
+++ src/hooks/usePreloader.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+
+const usePreloader = (...props:any):boolean => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const checkArgs = () => {
+    const isTruth = props.every((item:any) => {
+      switch (true) {
+        case Array.isArray(item):
+          return Boolean(item.length);
+        default:
+          return Boolean(item);
+      }
+    })
+   setIsLoaded(isTruth)
+  }
+
+  const disablePreloader = () => {
+    const preloader = document.querySelector('.preloader');
+
+    if(isLoaded && preloader) {
+      preloader.remove();
+    }
+  }
+
+  useEffect( () => {
+    checkArgs();
+    disablePreloader();
+  })
+
+  return isLoaded;
+}
+
+export default usePreloader;
