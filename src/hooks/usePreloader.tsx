17d43fa7fd27209ed18c9c79f790dce011b6b881
di-shu <dmitry.shulyachenko@gmail.com>
2021-04-20 14:35:45 +0300

    fixed checking data while getting from api

--- src/hooks/usePreloader.tsx
+++ src/hooks/usePreloader.tsx
@@ -1,23 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
-const usePreloader = (...props:any):boolean => {
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  const checkArgs = () => {
-    const isTruth = props.every((item:any):boolean => {
-      switch (true) {
-        case Array.isArray(item):
-          return !!item.length;
-        case item instanceof Object:
-          return !!Object.keys(item).length;
-        default:
-          return item ? true : false;
-      }
-    })
-   setIsLoaded(isTruth)
-  }
-
-  const disablePreloader = () => {
+const usePreloader = (isLoaded: boolean) => {
+  const checkData = (): void => {
     const preloader = document.querySelector('.preloader');
 
     if(isLoaded && preloader) {
@@ -26,11 +10,8 @@ const usePreloader = (...props:any):boolean => {
   }
 
   useEffect(() => {
-    checkArgs();
-    disablePreloader();
+    checkData();
   })
-
-  return isLoaded;
 }
 
 export default usePreloader;
