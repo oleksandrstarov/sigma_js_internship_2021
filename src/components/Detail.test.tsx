ec9246b5a6674be629ca947dcff6444a2753b390
Kach <kach_work@ukr.net>
2021-04-21 16:13:12 +0300

    minor fixes pre merge

--- src/components/Detail.test.tsx
+++ src/components/Detail.test.tsx
@@ -15,16 +15,14 @@ describe('Detail', () => {
     }
   });
 
-  afterAll(() => {
-    jest.clearAllMocks();
-  });
-
   it('should render with textContent prop', () => {
     expect(wrapper).toBeDefined();
   });
 
   it('should not render without textContent prop', () => {
     wrapper.setProps({ title: 'test', textContent: null });
+    wrapper.update();
+
     expect(wrapper.isEmptyRender()).toBe(true);
   });
 });
