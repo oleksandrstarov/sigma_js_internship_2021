4935c4513e4518df9d3237e0437b8260f5cebc07
Ilia Selieznov <ziziron21@gmail.com>
2021-04-22 18:01:00 +0300

    after pull

--- src/components/SettingsBar.tsx
+++ src/components/SettingsBar.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 
 import '../styles/Header.scss';
 import '../styles/SettingsBar.scss';
--- src/components/SettingsBtn.tsx
+++ src/components/SettingsBtn.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+// import { useState, useRef } from 'react';
 
 import useOutsideClick from 'src/hooks/useOutsideClick ';
 import '../styles/Header.scss';
