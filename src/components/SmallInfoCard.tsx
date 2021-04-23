a0a44ab4737fd74bb60a8adbd5ee583a8c5c4725
Ilia Selieznov <ziziron21@gmail.com>
2021-04-23 12:36:11 +0300

    fixed bug with 404 respose of img link request

--- src/components/SmallInfoCard.tsx
+++ src/components/SmallInfoCard.tsx
@@ -24,12 +24,7 @@ type ResponseData = {
 
 const SmallInfoCard = ({ id }: CardInfoProps) => {
   const { theme }: ThemeContextType = useContext(ThemeContext);
-  const [data, setData] = useState<ResponseData>({
-    poster_path: '',
-    vote_count: '',
-    title: '',
-    vote_average: ''
-  });
+  const [data, setData] = useState<ResponseData>();
 
   useEffect(() => {
     api.getDataById(id).then(res => {
@@ -37,7 +32,7 @@ const SmallInfoCard = ({ id }: CardInfoProps) => {
     });
   }, [id]);
 
-  return (
+  return <>{data && (
     <div className={`small-cardInfo ${theme ? '' : 'dark'}`}>
       <div className=" small-cardInfo__title">
         <Title text={data.title} />
@@ -73,7 +68,7 @@ const SmallInfoCard = ({ id }: CardInfoProps) => {
         </button>
       </Link>
     </div>
-  );
+  )}</>;
 };
 
 export default SmallInfoCard;
