f4a669b094f89e11f7e64436cd5906ad25d3d710
Ilia Selieznov <ziziron21@gmail.com>
2021-04-24 14:27:00 +0300

    fixed comments

--- src/hooks/useOutsideClick .tsx
+++ src/hooks/useOutsideClick .tsx
@@ -1,19 +1,19 @@
 import { useEffect, RefObject } from 'react';
 
 const useOutsideClick = (ref: RefObject<HTMLElement>, callback: () => void) => {
-    const handleClick = (e: any) => {
-        if (ref.current && !ref.current.contains(e.target)) {
-            callback();
-        }
+  const handleClick = (e: any) => {
+    if (ref.current && !ref.current.contains(e.target)) {
+      callback();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClick);
+
+    return () => {
+      document.removeEventListener('click', handleClick);
     };
-
-    useEffect(() => {
-        document.addEventListener('click', handleClick);
-
-        return () => {
-            document.removeEventListener('click', handleClick);
-        };
-    });
+  });
 };
 
 export default useOutsideClick;
