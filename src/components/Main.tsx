3d83710d69706d6b0c07da0da90e538c66baad2d
di-shu <dmitry.shulyachenko@gmail.com>
2021-04-25 09:23:43 +0300

    removed Main file from commit

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,17 +1,13 @@
 import { ReactNode } from 'react';
 
 import '../App.scss';
-import Breadcrumbs from "./Breadcrumbs";
 
 interface MainProps {
   children: ReactNode;
 }
 
 const Main = ({ children }: MainProps) => {
-  return <main className="main">
-    <Breadcrumbs/>
-    {children}
-  </main>;
+  return <main className="main">{children}</main>;
 };
 
 export default Main;
