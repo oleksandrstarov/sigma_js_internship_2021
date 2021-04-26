a58701819c6a59ae44ed93f51d413518a229ee55
Ilia Selieznov <ziziron21@gmail.com>
2021-04-26 10:41:30 +0300

    fixed comments

--- src/components/SettingsBtn.tsx
+++ src/components/SettingsBtn.tsx
@@ -7,28 +7,28 @@ import settings from '../assets/settings.png';
 import SettingsBar from './SettingsBar';
 
 const SettingsBtn = () => {
-    const [isBar, setIsBar] = useState(false);
-    const ref = useRef(null);
+  const [isBar, setIsBar] = useState(false);
+  const ref = useRef(null);
 
-    useOutsideClick(ref, () => {
-        if (isBar) {
-            setIsBar(false);
-        }
-    });
-
-    const hendlerBar = () => {
-        setIsBar(!isBar);
+  useOutsideClick(ref, () => {
+    if (isBar) {
+      setIsBar(false);
     }
-    return (
-        <div className="settings-wrapper">
-            <div onClick={() => hendlerBar()} className="header-nav-link settings-button">
-                <img src={settings} alt="settings img" className={!isBar ? "settings-img-button" : "settings-img-button-active"} />
-            </div>
-            <div ref={ref} className={!isBar ? "settings-bar" : "settings-bar-active"}>
-                <SettingsBar />
-            </div>
-        </div>
-    );
+  });
+
+  const hendlerBar = () => {
+    setIsBar(!isBar);
+  }
+  return (
+    <div className="settings-wrapper">
+      <div onClick={() => hendlerBar()} className="header-nav-link settings-button">
+        <img src={settings} alt="settings img" className={!isBar ? "settings-img-button" : "settings-img-button-active"} />
+      </div>
+      <div ref={ref} className={!isBar ? "settings-bar" : "settings-bar-active"}>
+        <SettingsBar />
+      </div>
+    </div>
+  );
 }
 
 export default SettingsBtn
