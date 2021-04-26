f27a5f21b6450c04f85573dfadb93017afdec9a1
Dmitriy Kurchenko <dmitriy1991dp@gmail.com>
2021-04-26 14:34:42 +0300

    minor changes in home and popular movie components

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -18,51 +18,61 @@ export type MoviesType = {
   overview: string;
   title: string;
   id: number;
-}
+};
 
 const Home = () => {
   const [movies, setMovies] = useState<[]>([]);
   const [history, setHistory] = useState<MoviesType[]>();
   const [popularMovies, setPopularMovies] = useState<Movie[]>([]);
-
+ 
   useEffect(() => {
     api.getPopularQueryList().then(res => {
       setMovies(res.slice(0, 10));
       setPopularMovies(res);
     });
-    api.getDataByIds(api.getHistoryIdList()).then((res) => { setHistory(res) });
+    api.getDataByIds(api.getHistoryIdList()).then(res => {
+      setHistory(res);
+    });
   }, []);
 
   return (
     <>
-     <MovieBanner popularMovies={popularMovies}/>
+      <MovieBanner popularMovies={popularMovies} />
       <div className="wrapper-space">
         <WrapperFavorites />
       </div>
       <Container>
-        {history && <div className="wrapper-space">
-          {history.length ? <><Title text={"Last seens"} />
+        {history && (
+          <div className="wrapper-space">
+            {history.length ? (
+              <>
+                <Title text={'Last seens'} />
+                <Slider>
+                  {history.map((movie: MoviesType) => {
+                    return (
+                      <div className="slide" key={movie.poster_path}>
+                        <SmallInfoCard id={movie.id} />
+                      </div>
+                    );
+                  })}
+                </Slider>{' '}
+              </>
+            ) : null}
+          </div>
+        )}
+        <div className="wrapper-space">
+          <Title text={'Popular movies'} />
+          {movies.length ? (
             <Slider>
-              {history.map((movie: MoviesType) => {
+              {movies.map((movie: MoviesType) => {
                 return (
                   <div className="slide" key={movie.poster_path}>
                     <SmallInfoCard id={movie.id} />
                   </div>
                 );
               })}
-            </Slider> </> : null}
-        </div>}
-        <div className="wrapper-space">
-          <Title text={'Popular movies'} />
-          {movies.length ? <Slider>
-            {movies.map((movie: MoviesType) => {
-              return (
-                <div className="slide" key={movie.poster_path}>
-                  <SmallInfoCard id={movie.id} />
-                </div>
-              );
-            })}
-          </Slider> : null}
+            </Slider>
+          ) : null}
         </div>
       </Container>
     </>
