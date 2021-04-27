9f482a6220c5dd455c0df51930d2f1b6f248dff8
mrSelieznov <41084902+mrSelieznov@users.noreply.github.com>
2021-04-27 13:20:24 +0300

    Feature/component bar (#182)
    * component fo settings
    
    * component fo settings
    
    * fixed comments
    
    * after pull
    
    * after pull
    
    * after pull
    
    * fixed comments
    
    * after pull
    
    * after pull
    
    * fixed comments
    
    * fixed comments
    
    * fix com.

---
+++ src/components/SettingsBtn.tsx
@@ -0,0 +1,35 @@
+import { useState, useRef } from 'react';
+
+import useOutsideClick from 'src/hooks/useOutsideClick ';
+import '../styles/Header.scss';
+
+import settings from '../assets/settings.png';
+import SettingsBar from './SettingsBar';
+
+const SettingsBtn = () => {
+  const [isSettingsBarActive, setIsSettingsBarActive] = useState(false);
+  const ref = useRef(null);
+
+  useOutsideClick(ref, () => {
+    if (isSettingsBarActive) {
+      setIsSettingsBarActive(false);
+    }
+  });
+
+  const handlerBar = () => {
+    setIsSettingsBarActive(!isSettingsBarActive);
+  }
+
+  return (
+    <div className="settings-wrapper">
+      <div onClick={() => handlerBar()} className="header-nav-link settings-button">
+        <img src={settings} alt="settings img" className={isSettingsBarActive ? "settings-img-button-active" : "settings-img-button"} />
+      </div>
+      <div ref={ref} className={isSettingsBarActive ? "settings-bar-active" : "settings-bar"}>
+        {isSettingsBarActive && <SettingsBar />}
+      </div>
+    </div>
+  );
+}
+
+export default SettingsBtn
